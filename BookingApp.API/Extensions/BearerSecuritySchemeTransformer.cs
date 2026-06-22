5d9ebec131e5138ae7ffdc4cc7a7d11c8fcaf5f4
CrANeZ2430 <crn.alexei@gmail.com>
2026-06-22 12:59:43 +0200

    feat(api): add global open api security scheme

---
+++ BookingApp.API/Extensions/BearerSecuritySchemeTransformer.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.OpenApi;
+using Microsoft.OpenApi;
+
+namespace BookingApp.API.Extensions;
+
+public sealed class BearerSecuritySchemeTransformer(
+    IAuthenticationSchemeProvider authenticationSchemeProvider) 
+    : IOpenApiDocumentTransformer
+{
+    public async Task TransformAsync(
+        OpenApiDocument document, 
+        OpenApiDocumentTransformerContext context, 
+        CancellationToken cancellationToken = default)
+    {
+        var authenticationSchemes = await authenticationSchemeProvider.GetAllSchemesAsync();
+        if (authenticationSchemes.Any(authScheme => authScheme.Name == "Bearer"))
+        {
+            document.Components ??= new OpenApiComponents();
+            document.Components.SecuritySchemes ??= new Dictionary<string, IOpenApiSecurityScheme>();
+            
+            var securityScheme = new OpenApiSecurityScheme()
+            {
+                Type = SecuritySchemeType.Http,
+                Scheme = "bearer",
+                In = ParameterLocation.Header,
+                BearerFormat = "JWT",
+                Description = "Enter your JWT token directly into the input box below."
+            };
+
+            document.Components.SecuritySchemes["Bearer"] = securityScheme;
+
+            var requirement = new OpenApiSecurityRequirement()
+            {
+                [new OpenApiSecuritySchemeReference("Bearer", document)] = new List<string>()
+            };
+            
+            document.Security ??= new List<OpenApiSecurityRequirement>();
+            document.Security.Add(requirement);
+        }
+    }
+}
--- BookingApp.API/Program.cs
+++ BookingApp.API/Program.cs
@@ -1,6 +1,8 @@
+using System.Collections.Immutable;
 using System.Text.Json.Serialization;
 using BookingApp.API.Conventions;
 using BookingApp.API.ExceptionHandling;
+using BookingApp.API.Extensions;
 using BookingApp.Application;
 using BookingApp.Infrastructure;
 using Microsoft.AspNetCore.Mvc.ApplicationModels;
@@ -19,16 +21,11 @@ builder.Services.AddAuthentication().AddJwtBearer(options =>
 {
     options.Authority = builder.Configuration["Auth0:Domain"];
     options.Audience = builder.Configuration["Auth0:Audience"];
-    // options.Events = new JwtBearerEvents()
-    // {
-    //     OnChallenge = context =>
-    //     {
-    //         context.HandleResponse();
-    //         throw new UnauthorizedException("Token is missing or invalid");
-    //     }
-    // };
 });
+
 builder.Services.AddAuthorization();
+
+builder.Services.AddOpenApi(options => options.AddDocumentTransformer<BearerSecuritySchemeTransformer>());
 
 builder.Services.AddControllers(options =>
         options.Conventions.Add(new RouteTokenTransformerConvention(new KebabCaseParameterTransformer())))
@@ -54,13 +51,12 @@ if (app.Environment.IsDevelopment())
     {
         options.WithTitle("Booking API")
             .WithTheme(ScalarTheme.DeepSpace)
-            .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient);
-        // options
-        //     .WithPreferredScheme("Bearer")
-        //     .WithHttpBearerAuthentication(bearer =>
-        //     {
-        //         bearer.Token = "your-token";
-        //     });
+            .WithDefaultHttpClient(ScalarTarget.CSharp, ScalarClient.HttpClient)
+            .AddPreferredSecuritySchemes("Bearer")
+            .AddHttpAuthentication("Bearer", http =>
+            {
+                http.Token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6IjNNS1ktTkV0aVZ2MEFXX1libkYtciJ9.eyJpc3MiOiJodHRwczovL2Rldi1jcm4uZXUuYXV0aDAuY29tLyIsInN1YiI6IlFjcDhXRHZDaEk1bms5V01qaEV3Ym1nRzhxVXBMaXFyQGNsaWVudHMiLCJhdWQiOiJodHRwczovL2xvY2FsaG9zdDo3MDc5IiwiaWF0IjoxNzgxOTY4NDA3LCJleHAiOjE3ODIwNTQ4MDcsImd0eSI6ImNsaWVudC1jcmVkZW50aWFscyIsImF6cCI6IlFjcDhXRHZDaEk1bms5V01qaEV3Ym1nRzhxVXBMaXFyIn0.M1WtAPbCe0QyfL-nyQija4XBsgAAk4bZrC4RXh9iNSghxkDAE9cTeMm47FMql4_pvqbPHXZxFY2KJRKkeuZkgSpxvgh3R_0WzMSoPSEfq36P9TrhRxiH9cqMvkhiJMLRkr3M84STz2_DqEkoyju5mv-OPKJrg4KqzpuwxqFk5D2ZxMsqRQJ5ll56hyxInvmadZuRqMueDSyTolC2o8qJpRQ80WxtKhvqldrK18-4_Xqc0JBn_ovAkLyJYua9vuxuwU-iCIqD9hdTFYcg-OleqjA89DPoD2SuGl2c1B0YXH2IK195rqmcD3yrA4KqrAHES-tNSBDS49oVUfp4QXcqEg";
+            });
     });
 }
 
